60a33b1c2e0ede25d2555c2f71a0e2636c61185e
Maxence Yang <jyunanyang@gmail.com>
2025-01-22 20:50:13 +0800

    sign up routing implementation

--- src/main/kotlin/idv/maxence2997/socialmediaapp/route/AuthRoute.kt
+++ src/main/kotlin/idv/maxence2997/socialmediaapp/route/AuthRoute.kt
@@ -3,6 +3,8 @@ package idv.maxence.idv.maxence2997.socialmediaapp.route
 import idv.maxence.idv.maxence2997.socialmediaapp.business.AuthBusinessService
 import idv.maxence.idv.maxence2997.socialmediaapp.domain.AuthResponse
 import idv.maxence.idv.maxence2997.socialmediaapp.domain.SignUpParams
+import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.routing.*
 import io.ktor.util.reflect.*
 import org.koin.ktor.ext.inject
@@ -11,20 +13,25 @@ fun Routing.authRouting() {
   val businessService by inject<AuthBusinessService>()
   route("/auth") {
     post("/sign-up") {
-      val params = call.receiveNullable<SignUpParams>(
-        typeInfo = TypeInfo(SignUpParams::class)
-      ) ?: return@post call.respond(
-        message = AuthResponse(
-          errorMessage = "Invalid request",
-        ),
-        typeInfo = TypeInfo(AuthResponse::class)
-      )
+      val params = call.parsePayload<SignUpParams>()
       val response = businessService.signUp(params)
 
       call.respond(
-        message = response.data,
-        typeInfo = TypeInfo(AuthResponse::class)
+        message = response.data, typeInfo = typeInfo<AuthResponse>()
       )
     }
+  }
+}
+
+suspend inline fun <reified T> ApplicationCall.parsePayload(
+  typeInfo: TypeInfo = typeInfo<T>(),
+): T {
+  return try {
+    this.receive<T>(typeInfo)
+  } catch (e: Exception) {
+    this.respond(
+      AuthResponse(errorMessage = "Invalid JSON: ${e.message}"), typeInfo = typeInfo<AuthResponse>()
+    )
+    throw e // rethrow to prevent further processing
   }
 }
